f939bb1234c1986f04c2ed93eb3ac5bd10486817
Tuan <nha.tuan84@gmail.com>
2017-05-06 19:45:01 +0700

    esp32lcd first version

---
+++ examples/esp32lcd/esp32lcd.ino
@@ -0,0 +1,23 @@
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{ 
+  lcd.begin(12, 14);                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(0,1);
+  lcd.print("by EasyIoT");
+}
+
+
+void loop()
+{
+}
